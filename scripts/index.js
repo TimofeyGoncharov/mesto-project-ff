2cdbf152f54df6411b050ee8d77bbd5fa1bc593c
Timofiey Goncharov <timofigoncharov6112@mail.ru>
2023-12-31 14:33:13 +0300

    first commit project

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
@@ -7,3 +8,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((item, error) => {
+    try {
+        createCard(item.name, item.link);
+    }
+    catch(error) {
+        console.log(error);
+    }
+})
+
+function createCard(name, link) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardOnline = document.querySelector('.places__list')
+
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+
+    cardOnline.append(cardElement);
+}
+
+function deleteCard(event) {
+    const cardItem = document.querySelector(`.${event.currentTarget.className}`).closest('.places__item');
+    cardItem.remove();
+}
